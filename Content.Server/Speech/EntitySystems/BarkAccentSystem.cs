2f1353e95998a2cffcb0876ba1c27140dc97b315
Southbridge <7013162+southbridge-fur@users.noreply.github.com>
2025-01-30 17:03:31 -0500

    checkpointing

--- Content.Server/Speech/EntitySystems/BarkAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/BarkAccentSystem.cs
@@ -1,14 +1,27 @@
 using Content.Server.Speech.Components;
 using Robust.Shared.Random;
+using System.Text.RegularExpressions;
 
 namespace Content.Server.Speech.EntitySystems
 {
     public sealed class BarkAccentSystem : EntitySystem
     {
         [Dependency] private readonly IRobustRandom _random = default!;
+        [Dependency] private readonly ReplacementAccentSystem _replacement = default!;
+
+        private static readonly Regex RegexTh = new(@"(?<=\s|^)th", RegexOptions.IgnoreCase);
+        private static readonly Regex RegexThe = new(@"(?<=\s|^)the(?=\s|$)", RegexOptions.IgnoreCase);
 
         private static readonly IReadOnlyList<string> Barks = new List<string>{
-            " Woof!", " WOOF", " wof-wof"
+            " Woof!", " WOOF", " wof-wof", " arf", " awrf"
+        }.AsReadOnly();
+
+        private static readonly IReadOnlyList<string> Questions = new List<string>{
+            ", arf?", ", awoo?", "...?"
+        }.AsReadOnly();
+
+        private static readonly IReadOnlyList<string> Comma = new List<string>{
+            ", "
         }.AsReadOnly();
 
         private static readonly IReadOnlyDictionary<string, string> SpecialWords = new Dictionary<string, string>()
@@ -17,6 +30,8 @@ namespace Content.Server.Speech.EntitySystems
             { "Ah", "Arf" },
             { "oh", "oof" },
             { "Oh", "Oof" },
+            { "uhh", "ruh" },
+            { "Uhh", "Ruh" }
         };
 
         public override void Initialize()
@@ -32,7 +47,9 @@ namespace Content.Server.Speech.EntitySystems
             }
 
             return message.Replace("!", _random.Pick(Barks))
-                .Replace("l", "r").Replace("L", "R");
+                .Replace("?", _random.Pick(Questions))
+                .Replace("l", "r").Replace("L", "R")
+
         }
 
         private void OnAccent(EntityUid uid, BarkAccentComponent component, AccentGetEvent args)
